bdb0151e3d9c7eeedcff2f48169782b3af004d75
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 03:52:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,19 @@ class Dog
     @name = dog_name
   end
   
-  def name = dog_name
+  def name=(dog_name)
     @name = dog_name
   end
   
-  def 
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
 end
