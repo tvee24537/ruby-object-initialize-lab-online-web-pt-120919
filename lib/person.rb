1298b9fa58b0b46ec0eb1281b9d4a13c8058e0d2
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 03:45:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  def initialize 
+  def initialize (person_name)
+    @name = person_name
+  end
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
 end
